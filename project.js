79cb7d5412dad351bf32d6cbde87139da6302061
gauravshirkegs <shirkegaurav611@gmail.com>
2022-10-01 22:27:44 +0530

    Web-101 project

--- project.js
+++ project.js
@@ -20,3 +20,21 @@ function tripsfun(){
     window.location.href="trips.html"
 }
 
+let signinKey=document.querySelector("#nav>div:nth-child(2)>button:nth-child(5)");
+signinKey.addEventListener("click", signinfun);
+function signinfun(){
+    window.location.href="Sign_in.html"
+}
+
+
+let sign= document.querySelector("#trips>div>div>button");
+sign.addEventListener("click", signfun);
+function signfun(){
+    window.location.href="Sign_in.html"
+}
+
+let log_in= document.querySelector("#trips>div>div>div");
+log_in.addEventListener("click",loginfun);
+function loginfun(){
+    window.location.href="IternarNum.html";
+}
